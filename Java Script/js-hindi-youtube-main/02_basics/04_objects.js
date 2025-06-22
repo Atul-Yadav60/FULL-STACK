65229db2ffda07ba12dd692801a4f6d4884b52b2
ATUL YADAV <bendpipe8@gmail.com>
2025-06-22 23:46:52 +0530

    practice

--- Java Script/js-hindi-youtube-main/02_basics/04_objects.js
+++ Java Script/js-hindi-youtube-main/02_basics/04_objects.js
@@ -1,6 +1,6 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object()  ->one way to create object
 
+const tinderUser = {} // another way to create object
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
@@ -9,6 +9,7 @@ tinderUser.isLoggedIn = false
 
 const regularUser = {
     email: "some@gmail.com",
+    // nested object
     fullname: {
         userfullname: {
             firstname: "hitesh",
@@ -18,18 +19,20 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
+ 
 
+// combine two objects
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 }  -> it give obj inside obj
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // it also just give obj combineed as array
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} // it just gave combination of two objs
 // console.log(obj3);
 
-
+// This is Array of Objects
 const users = [
     {
         id: 1,
@@ -48,11 +51,11 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));  -> it gives array of keys
+// console.log(Object.values(tinderUser));  -> it gives array of values
+// console.log(Object.entries(tinderUser));  -> it gives array of entries
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  -> it gives boolean value // use to check if the key is present in the object
 
 
 const course = {
@@ -63,16 +66,23 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course // it gives value of courseInstructor to instructor
 
 // console.log(courseInstructor);
 console.log(instructor);
+  
 
+// JSON -> JavaScript Object Notation . jason is also a object but it is in string format
+// Json also use to convert object into string and string into object
+
+// below is json object
 // {
 //     "name": "hitesh",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
+
+// it is also a array of objects
 
 [
     {},
--- Java Script/js-hindi-youtube-main/03_basics/01_functions.js
+++ Java Script/js-hindi-youtube-main/03_basics/01_functions.js
@@ -7,12 +7,12 @@ function sayMyName(){
     console.log("S");
     console.log("H");
 }
-
-// sayMyName()
+// sayMyName is just a function reference not a function call
+// sayMyName()  ->function call 
 
 // function addTwoNumbers(number1, number2){
 
-//     console.log(number1 + number2);
+//     console.log(number1 + number2); 
 // }
 
 function addTwoNumbers(number1, number2){
@@ -38,8 +38,10 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
 
-
+// ...num1 is a rest operator 
+// it is used to pass multiple arguments to a function and it will return an array of arguments
 function calculateCartPrice(val1, val2, ...num1){
+    // val1 and val2 are the first two arguments and num1 is the rest of the arguments that store rest values in array
     return num1
 }
 
@@ -49,7 +51,7 @@ const user = {
     username: "hitesh",
     prices: 199
 }
-
+ 
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
