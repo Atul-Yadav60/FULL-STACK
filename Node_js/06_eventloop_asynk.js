a64bc2a7a033c7153e175a021b2b042e34a17490
ATUL YADAV <bendpipe8@gmail.com>
2025-09-01 21:00:14 +0530

    npm tools

---
+++ Node_js/06_eventloop_asynk.js
@@ -0,0 +1,8 @@
+// Event Loop
+
+// Async Code
+// Async ka solution hai ki end code ko bh data code e roho jo code delay ho raha hai uske ander
+ 
+
+
+// SEE NOTES 
---
+++ Node_js/07_npm.js
@@ -0,0 +1,34 @@
+ const http = require('http')
+ const requesthandler = require('./06_eventloop_asynk')
+
+ const server = http.createServer((req,res)=>{
+  console.log(req)
+ })
+
+ const PORT = 3001
+
+ server.listen(PORT,()=>{
+  console.log(`Serveer running on address http://localhost:${PORT}`)
+ })
+
+
+
+//  hame npm ke bare me padha and node and nodemon kya hai khud ka package kaise banaye ye bhi padha :
+//  PS C:\Users\Atul2\OneDrive\Desktop\Rename\FULL STACK\Node_js> npm start
+
+// > npm-test@1.0.0 start
+// > node 07_npm.js
+
+// node:internal/modules/cjs/loader:1372
+//   throw err;
+//   ^
+// (C:\Users\Atul2\OneDrive\Desktop\Rename\FULL 
+// PS C:\Users\Atul2\OneDrive\Desktop\Rename\FULL STACK\Node_js> npm install nodemon --save-dav
+// npm warn Unknown cli config "--save-dav". This will stop working in the next major version of npm.
+
+// added 30 packages, and audited 31 packages in 3s
+
+// 4 packages are looking for funding
+//   run `npm fund` for details
+
+// found 0 vulnerabilities
